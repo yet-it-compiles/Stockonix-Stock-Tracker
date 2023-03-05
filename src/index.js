50fd4514707c8ad69ff5af863b00776eb7222878
Tyler S. Unsworth <bmxfreestyle357@gmail.com>
2023-03-05 01:36:57 -0800

    Refactored widget to better fit its purpose

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import WidgetInitialState from './components/dashboard/Widget';
 
 // Declares the root of the 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   // <React.StrictMode>
-  <App />
+  <WidgetInitialState />
   // </React.StrictMode>
 );
