3c578ef7907d2d11e2a7bc058b46981b2b7e91de
Tyler S. Unsworth <bmxfreestyle357@gmail.com>
2023-03-05 00:45:17 -0800

    Created initial widget display, and finished styling.

--- src/components/dashboard/App.js
+++ src/components/dashboard/App.js
@@ -1,15 +1,35 @@
 import React, { useState } from "react";
+import AlphaVantageAPI from '../../scripts/AlphaVantageAPI.js';
+
 import './dashboard.css';
 
 
 /**
- * @todo This is a hardcoded example of what a widget would look like.
- * This 
+ * 
+ * @returns 
+ */
+const WidgetInitialState = () => {
+
+  const handleStockPrompt = () => {
+    const insertNewStock = window.prompt("Enter the name of the stock:");
+  }
+
+  return (
+    <div id="stock-container-initial">
+      <h1 id="add-stock" onClick={handleStockPrompt}>+</h1>
+    </div>
+  )
+}
+
+
+
+
+
+/**
+ * 
  * @returns 
  */
 const WidgetInteraction = () => {
-
-  // 
   const [widgetClicked, setWidget] = useState(false);
 
   const handleSettingWidget = () => {
@@ -18,6 +38,11 @@ const WidgetInteraction = () => {
 
   return (
     <div id="stock-container">
+      {/* <input type="button" id="edit-widget-button" class="button"></input> */}
+      <input type="button" id="edit-widget-button" class="button" value="✍🏽"></input>
+
+      <input type="button" id="navigate-to-metrics-button" class="button" value="📉"></input>
+
       <h1 id="stock-symbol">TSLA</h1>
 
       <h2 id="current-price" class="type-number">$202.77
@@ -25,9 +50,9 @@ const WidgetInteraction = () => {
 
       <h3 id="price-change" class="type-number">+$4.12
         <span id="percent-change" class="type-number"> (+7.53%)</span></h3>
-      <h4 id="last-updated" class="type-number">Last Updated: 03/04/2022</h4>
+      <h4 id="last-updated">Last Updated: 03/04/2022</h4>
     </div>
   )
 }
 
-export default WidgetInteraction;
+export default WidgetInitialState;
