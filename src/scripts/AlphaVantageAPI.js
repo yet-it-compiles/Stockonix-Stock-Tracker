c2b902f55fb474adf31079bed0a75b58dd8e5e88
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2023-03-30 08:30:55 -0700

    Reduced Runtime Complexity
    Runtime complexity was reduced in both class methods

--- src/scripts/AlphaVantageAPI.js
+++ src/scripts/AlphaVantageAPI.js
@@ -1,50 +1,58 @@
+import AlphaVantage from 'alphavantage';
+
 /**
  * This module establishes an API connection with AlphaVantage, and instantiates
  * a class to retrieve the user requested stock information.
  * 
- * The AlphaVantageAPI class takes in a stock symbol as an argument through 
- * its constructor, and defines methods for retrieving the stock information.
+ * The AlphaVantageAPI class takes in a stock symbol as an argument through its
+ * constructor, and defines methods for retrieving the stock information.
  * 
  * @class AlphaVantageAPI
  */
+export default class AlphaVantageAPI {
 
-export class AlphaVantageAPI {
-    constructor(requestedStock = "TSLA") {
+    /**
+     * Creates an instance of AlphaVantageAPI
+     * 
+     * 
+     * @param {string} requestedStock The stock symbol to retrieve metrics for
+     */
+    constructor(requestedStock = 'TSLA') {
         this.requestedStock = requestedStock;
-        this.alpha = require('alphavantage')({ key: 'WX5GQNRSWDWX1NZT' });
+        this.alpha = new AlphaVantage({ key: 'WX5GQNRSWDWX1NZT' });
     }
 
     /**
-     * Retrieves the requested stocks information as a promises, and resolves 
+     * Retrieves the requested stock's information as a promise, and resolves 
      * the values to a destructured object.
      * 
-     * @returns {Promise} Resolved destructured object containing the stocks: 
-     * symbol, open, high, low, current price, volume, and previousClose
+     * @returns {Promise} Resolved destructured object containing the symbol, 
+     * open, high, low, current price, volume, and previous close.
      */
     async getStockInformation() {
-        const data = await this.alpha.data.quote(this.requestedStock);
-
-        const { '01. symbol': symbol, '02. open': open, '03. high': high,
-            '04. low': low, '05. price': currentPrice, '06. volume': volume,
-            '08. previous close': previousClose } = data['Global Quote'];
-
+        const { 'Global Quote': {
+            '01. symbol': symbol,
+            '02. open': open,
+            '03. high': high,
+            '04. low': low,
+            '05. price': currentPrice,
+            '06. volume': volume,
+            '08. previous close': previousClose
+        } } = await this.alpha.data.quote(this.requestedStock);
         return { symbol, open, high, low, currentPrice, volume, previousClose };
     }
 
     /**
-     * Retrieves the requested stocks company information as a promise, and 
+     * Retrieves the requested stock's company information as a promise, and 
      * resolves the values to a destructured object.
      * 
-     * @returns {Promise} Resolved destructured object containing the stocks: 
-     * descriptions, PERatio, and EPS.
+     * @returns {Promise} Resolved destructured object containing the company 
+     * description, PE Ratio, EPS, and beta.
      */
     async getCompanyInformation() {
-        const data = await this.alpha.fundamental.company_overview(this.requestedStock);
         const { 'Description': companyDescription, 'PERatio': PERatio,
-            EPS, 'Beta': beta } = data;
-
+            'EPS': EPS, 'Beta': beta } =
+            await this.alpha.fundamental.company_overview(this.requestedStock);
         return { companyDescription, PERatio, EPS, beta };
     }
-}
-
-export default AlphaVantageAPI;
+};
