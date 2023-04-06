6e94b07294b39739ff715e455717c2ab360b3e2d
Tyler S. Unsworth <43221618+yet-it-compiles@users.noreply.github.com>
2023-04-06 05:22:46 -0700

    Extended modals functionality based on interaction
    Defined a react-modal that opens when the default widget is interacted with, and displays a popup window that requests the user to enter a stock symbol.

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,22 +6,59 @@
  * @requires './widgets.css'
  */
 
-import React from "react";
-import './widgets.css';
+import React, { useState } from "react";
+import DisplayModal from "../modal/DisplayModal";
+import "./widgets.css";
 
 /**
- * Displays a grid of ten widgets, separated in two rows of five
- * @returns JSX element representing the dashboards initial layout
+ * Defines a default widget, and assigns a modal to request a stock 
+ * symbol for the user 
+ * 
+ * @returns a default widget intractable widget 
+ */
+const DefaultWidget = () => {
+    // Determines if the modal is currently opened or closed
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    /**
+     * An event handler that enables the modal to open when the user 
+     * interacts with a default widget.
+     */
+    const handleOpenModal = () => {
+        setIsModalOpen(() => true);
+    };
+
+    /**
+     * An event handler that closes the modal window on request
+     */
+    const handleCloseModal = () => {
+        setIsModalOpen(() => false);
+    };
+
+    return (
+        <>
+            <div className="widget-default" onClick={handleOpenModal}>
+                <p>Add Stock</p>
+            </div>
+
+            <DisplayModal isOpen={isModalOpen} isClosed={handleCloseModal} />
+        </>
+    );
+};
+
+/**
+ * Initializes a dense array of length 10, representing each widget 
+ * and assigns them a unique key determined by their index.
+ * 
+ * @returns JSX element representing the dashboards initial 5x2 layout
  */
 const StockonixDashboard = () => {
 
     /**
-     * Declares an dense array of length 10, representing each widget
+     * Declares a dense array of length 10, representing each widget
      */
     const displayWidgets = Array.from({ length: 10 }, (_, index) => (
-        <div className='widget-default' widgetId={index + 1} key={index}>
-            <p>Add Stock</p>
-        </div>
+        <DefaultWidget widgetid={index++} key={index} />
     ));
 
     return <div className="grid-layout">{displayWidgets}</div>;
